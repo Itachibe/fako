2e97291ce1af1bdc1473edcd1345774bd7b0fbbe
Itachibe <128122584+Itachibe@users.noreply.github.com>
2023-03-17 08:46:14 +0100

    Add files via upload

---
+++ 1/CrawlSearchEngine.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import codecs
+import bing , google , yahoo
+import re
+
+def crawler(keyword):
+
+    searchEngileList =[google,bing,yahoo]
+    searchresult = list()
+    hreflist = set()
+    repeatlist = list()
+
+    for se in searchEngileList:
+
+
+        for eachresult in se.crawler(keyword):
+
+
+            r = re.compile("(?:https?://)?"+eachresult["href"])
+            newlist = list(filter(r.match, hreflist))
+
+            if len(newlist) < 1 :
+
+                print(eachresult["title"])
+                print(eachresult["href"])
+                print(eachresult["brief"])
+                print("\n","-----------------------------------\n")
+
+                hreflist.add(eachresult["href"])
+                searchresult.append(eachresult)
+
+            else:
+                repeatlist.append(eachresult["href"])
+                print("repeat se: "+str(se)+eachresult["href"])
+        
+    print("non repeat: "+str(len(searchresult)))
+
+    for i in repeatlist:
+        print(i)
+        
+    return searchresult
+
+
+
+if __name__ == '__main__':
+
+    crawler("ekaroka")
+
+
+    
---
+++ 1/yahoo.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import codecs
+import re
+
+def crawler(keyword):
+
+    searchresult = list()
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3495.100 Safari/537.36 OPR/56.0.3041.43'}
+
+    res=requests.get("https://tw.search.yahoo.com/search?p="+keyword,headers=headers)
+    # res = requests.get("https://tw.search.yahoo.com/search;_ylt=AwrtEFhPdNNbChAA28Nr1gt.;_ylu=X3oDMTFhdTV0M3ZoBGNvbG8DdHcxBHBvcwMxBHZ0aWQDQjU1MTlfMQRzZWMDcGFnaW5hdGlvbg--?p=searchengine&pz=14&bct=0&b=1&pz=10&bct=0&xargs=0")
+
+    parser = BeautifulSoup(res.text,"html.parser")
+
+    searchresultlist = parser.find('ol',class_="searchCenterMiddle").findAll('li',recursive=False)
+    print("yahoo: "+str(len(searchresultlist)))
+
+    for sr in searchresultlist:
+
+       eachresult = OrderedDict()
+        
+       eachresult['title'] = sr.find('h3').text       
+       # print(eachresult['title'])
+
+
+       if sr.find(class_="aUrl") is None:
+        eachresult['href'] = sr.find('a')['href']
+        # print(eachresult['title'])
+       else:
+        eachresult['href'] = sr.find(class_="aUrl").text
+        # print(eachresult['title'])
+
+
+       if sr.find(class_='options-toggle') is not None:
+        eachresult['brief'] = sr.find(class_="lh-l").text
+        # print(eachresult['brief'])
+       else:
+        eachresult['brief'] = ""
+        # print(eachresult['brief'])
+
+       searchresult.append(eachresult)
+
+       # print("---------------------------------------------")
+    return searchresult
+
+
+
+if __name__ == '__main__':
+    crawler("ekaroka")
